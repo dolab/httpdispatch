f04eab8e48261a8170ef0ab53ee1613f95f06c43
Spring MC <Heresy.MC@gmail.com>
2018-03-26 11:21:54 +0800

    What: refactor context params key
    Why:
    
      * rename ctxParamKey to ctxParamHeaderKey
    
    How:
    
      * nop

--- context.go
+++ context.go
@@ -1,10 +1,7 @@
 package httpdispatch
 
-import "fmt"
-
 var (
-	ctxParam    = ctxKey{}                              // for http.Request.Context() introduced from go 1.7
-	ctxParamKey = fmt.Sprintf("X-Params-%p", &ctxParam) // for go <1.7
+	ctxParamKey = ctxParam{} // for http.Request.Context() introduced from go 1.7
 )
 
-type ctxKey struct{}
+type ctxParam struct{}
--- dispatcher_1_6.go
+++ dispatcher_1_6.go
@@ -6,30 +6,20 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/gob"
+	"fmt"
 	"net/http"
 )
 
-// Handler is an adapter which allows the usage of a http.Handler as a
-// request handle.
-func (r *Dispatcher) Handler(method, path string, handler http.Handler) {
-	r.Handle(method, path,
-		func(w http.ResponseWriter, req *http.Request, ps Params) {
-			buf := bytes.NewBuffer(nil)
-			if err := gob.NewEncoder(buf).Encode(ps); err == nil {
-				req.Header.Add(ctxParamKey, base64.RawURLEncoding.EncodeToString(buf.Bytes()))
-			}
-
-			handler.ServeHTTP(w, req)
-		},
-	)
-}
+var (
+	ctxParamHeaderKey = fmt.Sprintf("X-Params-%p", &ctxParamKey) // for go <1.7
+)
 
 // ContextParams pulls the URL parameters from a request context,
 // or returns nil if none are present.
 //
 // This is only present for go <1.7.
 func ContextParams(r *http.Request) Params {
-	value := r.Header.Get(ctxParamKey)
+	value := r.Header.Get(ctxParamHeaderKey)
 	if value == "" {
 		return nil
 	}
@@ -45,4 +35,19 @@ func ContextParams(r *http.Request) Params {
 	}
 
 	return params
+}
+
+// Handler is an adapter which allows the usage of a http.Handler as a
+// request handle.
+func (r *Dispatcher) Handler(method, path string, handler http.Handler) {
+	r.Handle(method, path,
+		func(w http.ResponseWriter, req *http.Request, ps Params) {
+			buf := bytes.NewBuffer(nil)
+			if err := gob.NewEncoder(buf).Encode(ps); err == nil {
+				req.Header.Add(ctxParamHeaderKey, base64.RawURLEncoding.EncodeToString(buf.Bytes()))
+			}
+
+			handler.ServeHTTP(w, req)
+		},
+	)
 }
